effd97ae841dc7107ad639f5b3fe3d38a245921d
mateo29dm <mateo.duque@correounivalle.edu.co>
2024-04-24 12:30:31 -0500

    Se arreglan las texturas del mapa 1 mediante blender, se remueven las texturas por codigo

--- src/world/Map1.jsx
+++ src/world/Map1.jsx
@@ -5,57 +5,53 @@ import { RepeatWrapping } from 'three'
 
 export default function Map1(props) {
     const { nodes, materials } = useGLTF('/assets/models/world/Map1.glb');
-    const FLOOR_PATH = '/assets/textures/floor/';
-    const WALL_PATH = '/assets/textures/walls/';
+    // const FLOOR_PATH = '/assets/textures/floor/';
+    // const WALL_PATH = '/assets/textures/walls/';
 
-    const propsFloorTexture = useTexture({
-        map: FLOOR_PATH + 'rock_path_diff_1k.jpg',
-        displacementMap: FLOOR_PATH + 'rock_path_disp_1k.png',
-        normalMap: FLOOR_PATH + 'rock_path_nor_gl_1k.jpg',
-        roughnessMap: FLOOR_PATH + 'rock_path_rough_1k.jpg',
-    });
+    // const propsFloorTexture = useTexture({
+    //     map: FLOOR_PATH + 'rock_path_diff_1k.jpg',
+    //     displacementMap: FLOOR_PATH + 'rock_path_disp_1k.png',
+    //     normalMap: FLOOR_PATH + 'rock_path_nor_gl_1k.jpg',
+    //     roughnessMap: FLOOR_PATH + 'rock_path_rough_1k.jpg',
+    // });
 
-    const propsWallTexture = useTexture({
-        map: WALL_PATH + 'plastered_stone_wall_diff_1k.jpg',
-        displacementMap: WALL_PATH + 'plastered_stone_wall_disp_1k.png',
-        normalMap: WALL_PATH + 'plastered_stone_wall_nor_gl_1k.jpg',
-        roughnessMap: WALL_PATH + 'plastered_stone_wall_rough_1k.jpg',
-    });
+    // const propsWallTexture = useTexture({
+    //     map: WALL_PATH + 'plastered_stone_wall_diff_1k.jpg',
+    //     displacementMap: WALL_PATH + 'plastered_stone_wall_disp_1k.png',
+    //     normalMap: WALL_PATH + 'plastered_stone_wall_nor_gl_1k.jpg',
+    //     roughnessMap: WALL_PATH + 'plastered_stone_wall_rough_1k.jpg',
+    // });
 
 
-    propsFloorTexture.map.repeat.set(4, 64);
-    propsFloorTexture.map.wrapS = propsFloorTexture.map.wrapT = RepeatWrapping;
+    // propsFloorTexture.map.repeat.set(4, 64);
+    // propsFloorTexture.map.wrapS = propsFloorTexture.map.wrapT = RepeatWrapping;
 
-    propsFloorTexture.map.repeat.set(4, 64);
-    propsFloorTexture.map.wrapS = propsFloorTexture.normalMap.wrapT = RepeatWrapping;
+    // propsFloorTexture.map.repeat.set(4, 64);
+    // propsFloorTexture.map.wrapS = propsFloorTexture.normalMap.wrapT = RepeatWrapping;
 
-    propsFloorTexture.map.repeat.set(4, 64);
-    propsFloorTexture.map.wrapS = propsFloorTexture.roughnessMap.wrapT = RepeatWrapping;
+    // propsFloorTexture.map.repeat.set(4, 64);
+    // propsFloorTexture.map.wrapS = propsFloorTexture.roughnessMap.wrapT = RepeatWrapping;
     
-    propsWallTexture.map.repeat.set(4, 64);
-    propsWallTexture.map.wrapS = propsWallTexture.displacementMap.wrapT = RepeatWrapping;
+    // propsWallTexture.map.repeat.set(4, 64);
+    // propsWallTexture.map.wrapS = propsWallTexture.displacementMap.wrapT = RepeatWrapping;
 
-    propsWallTexture.map.repeat.set(4, 64);
-    propsWallTexture.map.wrapS = propsWallTexture.map.wrapT = RepeatWrapping;
+    // propsWallTexture.map.repeat.set(4, 64);
+    // propsWallTexture.map.wrapS = propsWallTexture.map.wrapT = RepeatWrapping;
 
-    propsWallTexture.map.repeat.set(4, 64);
-    propsWallTexture.map.wrapS = propsWallTexture.normalMap.wrapT = RepeatWrapping;
+    // propsWallTexture.map.repeat.set(4, 64);
+    // propsWallTexture.map.wrapS = propsWallTexture.normalMap.wrapT = RepeatWrapping;
 
-    propsWallTexture.map.repeat.set(4, 64);
-    propsWallTexture.map.wrapS = propsWallTexture.roughnessMap.wrapT = RepeatWrapping;
+    // propsWallTexture.map.repeat.set(4, 64);
+    // propsWallTexture.map.wrapS = propsWallTexture.roughnessMap.wrapT = RepeatWrapping;
     
-    propsWallTexture.map.repeat.set(4, 64);
-    propsWallTexture.map.wrapS = propsWallTexture.displacementMap.wrapT = RepeatWrapping;
+    // propsWallTexture.map.repeat.set(4, 64);
+    // propsWallTexture.map.wrapS = propsWallTexture.displacementMap.wrapT = RepeatWrapping;
 
     return (
         <group {...props} dispose={null}>
           <group>
-            <mesh geometry={nodes.Walls.geometry}>
-                <meshStandardMaterial {...propsWallTexture}/>
-                </mesh>
-            <mesh geometry={nodes.Floor.geometry}>
-                <meshStandardMaterial {...propsFloorTexture}/>
-                </mesh>
+            <mesh geometry={nodes.Walls.geometry} material={materials.WallMaterial} />
+            <mesh geometry={nodes.Floor.geometry} material={materials.FloorMaterial}/>
           </group>
         </group>
       )
