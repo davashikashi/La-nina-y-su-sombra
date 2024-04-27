9781ee32f2c3563e023f737e4d83f59fe4731142
davashikashi <deison.cardona@correounivalle.edu.co>
2024-04-27 00:51:20 -0500

    level1 route

--- src/routes/RoutesLaNinaYSuSombra.jsx
+++ src/routes/RoutesLaNinaYSuSombra.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Level3 from "../pages/level3/Level3"
+import Level1 from "../pages/Level1/Level1"
+
 
 export default function RoutesLaNinaYSuSombra(){ 
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/level3" element={<Level3/>} />
+                <Route path="/level1" element={<Level1/>} />
             </Routes>
         </BrowserRouter>
     )
