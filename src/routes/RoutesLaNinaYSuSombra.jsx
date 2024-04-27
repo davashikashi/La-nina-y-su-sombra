64d7b3f5383abd13d4570b9a2fc1a44d194b1534
davashikashi <deison.cardona@correounivalle.edu.co>
2024-04-27 02:55:37 -0500

    login page route

--- src/routes/RoutesLaNinaYSuSombra.jsx
+++ src/routes/RoutesLaNinaYSuSombra.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Level3 from "../pages/level3/Level3"
 import Level1 from "../pages/Level1/Level1"
+import Login from "../pages/login/Login"
 
 
 export default function RoutesLaNinaYSuSombra(){ 
     return(
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Login/>}/>
                 <Route path="/level3" element={<Level3/>} />
                 <Route path="/level1" element={<Level1/>} />
             </Routes>
