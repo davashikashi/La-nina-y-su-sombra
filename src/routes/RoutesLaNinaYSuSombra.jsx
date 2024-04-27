01f3006f7e9a8db2b3c16b6a930dd29883b0eb69
WiloxW <wilian.quintero@correounivalle.edu.co>
2024-04-27 08:01:03 -0500

    Agregado el nivel de la sombra (Requiere enrutamiento)

--- src/routes/RoutesLaNinaYSuSombra.jsx
+++ src/routes/RoutesLaNinaYSuSombra.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Level3 from "../pages/level3/Level3"
+import Level2 from "../pages/leveldark/Leveldark.jsx"
 
 export default function RoutesLaNinaYSuSombra(){ 
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/level3" element={<Level3/>} />
+                <Route path="/level2" element={<Level2/>} />
             </Routes>
         </BrowserRouter>
     )
