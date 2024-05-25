d8cf8ac98a6d3955a7b2f7382585f697b5556447
davashikashi <deison.cardona@correounivalle.edu.co>
2024-05-24 22:49:41 -0500

    musica al Map2

--- src/pages/level2/world/Map2.jsx
+++ src/pages/level2/world/Map2.jsx
@@ -1,26 +1,47 @@
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react';
+import { useGLTF } from '@react-three/drei';
 import { RigidBody, CuboidCollider } from '@react-three/rapier';
 import Floor from "../floor/Floor";
 import Walls from "../walls/Walls";
 import { useGameContext } from '../../../context/GameContext';
-import golpeado from "../../../Sounds/hitEnemigo.mp3"
+import golpeado from "../../../Sounds/hitEnemigo.mp3";
+import cancion from "../../../Sounds/Main Theme.mp3";
 
 export default function Map2(props) {
-  const { nodes, materials } = useGLTF('/assets/models/world/Map2.glb')
-  const { setHealth } = useGameContext()
+  const { nodes, materials } = useGLTF('/assets/models/world/Map2.glb');
+  const { shadowAvatar, setHealth } = useGameContext();
 
-  const hitEnemigo = new Audio(golpeado)
+  const hitEnemigoRef = useRef(new Audio(golpeado));
+  const cancionThemeRef = useRef(new Audio(cancion));
+
+  useEffect(() => {
+    const cancionTheme = cancionThemeRef.current;
+    cancionTheme.loop = true;
+    cancionTheme.volume = 0.15;
+
+    const playAudio = () => {
+      cancionTheme.play().catch(error => {
+        console.error('Error playing audio:', error);
+      });
+    };
+
+    document.addEventListener('click', playAudio, { once: true });
+
+    return () => {
+      document.removeEventListener('click', playAudio);
+      cancionTheme.pause();
+    };
+  }, [shadowAvatar]);
 
   const handleFalling = (event) => {
-    console.log(event.colliderObject.name)
+    console.log(event.colliderObject.name);
     if (event.colliderObject.name === "character-capsule-collider") {
+      const hitEnemigo = hitEnemigoRef.current;
       hitEnemigo.volume = 0.3;
       hitEnemigo.play();
       setHealth(prevHealth => prevHealth - 3);
     }
-  }
-
+  };
 
   return (
     <group {...props} dispose={null}>
@@ -38,10 +59,9 @@ export default function Map2(props) {
             geometry={nodes.Floor.geometry}
             materials={materials.floorMaterial} />
         </RigidBody>
-
       </group>
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/assets/models/world/Map2.glb')
+useGLTF.preload('/assets/models/world/Map2.glb');
