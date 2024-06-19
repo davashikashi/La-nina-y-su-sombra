33e751e92ef09ba543c782a08665a2e358b030c0
davashikashi <deison.cardona@correounivalle.edu.co>
2024-06-19 14:32:13 -0500

    Update Avatar.jsx

--- src/characters/avatar/Avatar.jsx
+++ src/characters/avatar/Avatar.jsx
@@ -48,64 +48,81 @@ const Avatar = forwardRef((props, ref) => {
     //console.log("huesos", nodes.GirlModel.skeleton)
 
 
-    //control de teclas
-    const handleKeyDown = (event) => {
-        if (event.key.toLowerCase() === "f") {
-            //console.log("con colisiones")
-            setIsAttacking(true)
-            //console.log("colisiones") // Habilita colisiones
-            lanzaGolpe.volume = 0.1;
-            lanzaGolpe.play();
-            setCollisionEndTime(Date.now() + 1000); // Deshabilitar en 1.5 segundos
-        }
+    // //control de teclas
+    // const handleKeyDown = (event) => {
+    //     if (event.key.toLowerCase() === "f") {
+    //         //console.log("con colisiones")
+    //         setIsAttacking(true)
+    //         //console.log("colisiones") // Habilita colisiones
+    //         lanzaGolpe.volume = 0.1;
+    //         lanzaGolpe.play();
+    //         setCollisionEndTime(Date.now() + 1000); // Deshabilitar en 1.5 segundos
+    //     }
 
-        if (event.key.toLowerCase() === "w"
-            || event.key.toLowerCase() === "a"
-            || event.key.toLowerCase() === "s"
-            || event.key.toLowerCase() === "d") {
-            andando.loop = true
-            andando.volume = 0.2;
-            andando.play();
-        }
-        if (event.key.toLowerCase() === "q") {
+    //     if (event.key.toLowerCase() === "w"
+    //         || event.key.toLowerCase() === "a"
+    //         || event.key.toLowerCase() === "s"
+    //         || event.key.toLowerCase() === "d") {
+    //         andando.loop = true
+    //         andando.volume = 0.2;
+    //         andando.play();
+    //     }
+    //     if (event.key.toLowerCase() === "q") {
 
-            setPequeño((prevPequeño) => !prevPequeño) //que bendicion papa Dios
+    //         setPequeño((prevPequeño) => !prevPequeño) //que bendicion papa Dios
 
-        }
-
-
-    };
+    //     }
 
 
-
-    const handleKeyUp = (event) => {
-        if (event.key.toLowerCase() === "w" || event.key.toLowerCase() === "a" || event.key.toLowerCase() === "s" || event.key.toLowerCase() === "d") {
-            andando.pause();
-            andando.currentTime = 0;
-        }
-    };
+    // };
 
     useEffect(() => {
-        window.addEventListener("keydown", handleKeyDown);
-        window.addEventListener("keyup", handleKeyUp);
+        // Setup the WebSocket event listener for "player-moving"
+        socket.on("player-moving", (transforms) => {
+            const { translation, rotation } = transforms;
 
+            const newTranslation = new Vector3(...translation);
+            const newRotation = new Quaternion(...rotation);
+
+            avatarBodyRef.current?.setTranslation(newTranslation);
+            avatarBodyRef.current?.setRotation(newRotation);
+        });
+
+        // Clean up the event listener on component unmount
         return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-            window.removeEventListener("keyup", handleKeyUp);
+            socket.off("player-moving");
         };
     }, []);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (collisionEndTime && Date.now() > collisionEndTime) {
-                //console.log("sin colisiones")
-                setIsAttacking(false);
-                setCollisionEndTime(null);
-            }
-        }, 100);
 
-        return () => clearInterval(interval);
-    }, [collisionEndTime]);
+    // const handleKeyUp = (event) => {
+    //     if (event.key.toLowerCase() === "w" || event.key.toLowerCase() === "a" || event.key.toLowerCase() === "s" || event.key.toLowerCase() === "d") {
+    //         andando.pause();
+    //         andando.currentTime = 0;
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     window.addEventListener("keydown", handleKeyDown);
+    //     window.addEventListener("keyup", handleKeyUp);
+
+    //     return () => {
+    //         window.removeEventListener("keydown", handleKeyDown);
+    //         window.removeEventListener("keyup", handleKeyUp);
+    //     };
+    // }, []);
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         if (collisionEndTime && Date.now() > collisionEndTime) {
+    //             //console.log("sin colisiones")
+    //             setIsAttacking(false);
+    //             setCollisionEndTime(null);
+    //         }
+    //     }, 100);
+
+    //     return () => clearInterval(interval);
+    // }, [collisionEndTime]);
 
 
     //keyboards y animationsets
@@ -134,7 +151,7 @@ const Avatar = forwardRef((props, ref) => {
         { name: "action4", keys: ["KeyF"] }//tecla para atacar
     ]
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    // ///////////////////////////////////////////////////////////////////////////////////////////////////
 
     // useEffect(() => {
     //     setActualposition(avatarBodyRef.current?.translation())
@@ -142,11 +159,11 @@ const Avatar = forwardRef((props, ref) => {
 
     // },[avatarBodyRef.current?.translation()]);
 
-    useEffect(() => {
-        setShadowAvatar(
-            avatarBodyRef.current
-        )
-    }, [avatarBodyRef?.current])
+    // useEffect(() => {
+    //     setShadowAvatar(
+    //         avatarBodyRef.current
+    //     )
+    // }, [avatarBodyRef?.current])
 
 
     // useEffect(() => {
@@ -157,89 +174,89 @@ const Avatar = forwardRef((props, ref) => {
 
 
 
-    useFrame(() => {
+    // useFrame(() => {
 
-        if (handRefCollider.current) {
-            // Obtén la posición y rotación del props.props.rightHandBone
-            const worldPosition = rightHandBone.getWorldPosition(new Vector3());
-            const worldQuaternion = rightHandBone.getWorldQuaternion(new Quaternion());
+    //     if (handRefCollider.current) {
+    //         Obtén la posición y rotación del props.props.rightHandBone
+    //         const worldPosition = rightHandBone.getWorldPosition(new Vector3());
+    //         const worldQuaternion = rightHandBone.getWorldQuaternion(new Quaternion());
 
-            const realtivePosition = handOffset.clone().applyQuaternion(worldQuaternion)
+    //         const realtivePosition = handOffset.clone().applyQuaternion(worldQuaternion)
 
-            // Ajusta la posición y rotación del rigidbody del HandSword
-            const adjustedPosition = new Vector3(
-                worldPosition.x + realtivePosition.x,
-                worldPosition.y + realtivePosition.y,
-                worldPosition.z + realtivePosition.z
-            )
+    //         Ajusta la posición y rotación del rigidbody del HandSword
+    //         const adjustedPosition = new Vector3(
+    //             worldPosition.x + realtivePosition.x,
+    //             worldPosition.y + realtivePosition.y,
+    //             worldPosition.z + realtivePosition.z
+    //         )
 
-            handRefCollider.current.setTranslation({
-                x: adjustedPosition.x,
-                y: adjustedPosition.y,
-                z: adjustedPosition.z
-            });
+    //         handRefCollider.current.setTranslation({
+    //             x: adjustedPosition.x,
+    //             y: adjustedPosition.y,
+    //             z: adjustedPosition.z
+    //         });
 
-            const adjustedRotation = worldQuaternion.clone().multiply(rotationOffset);
+    //         const adjustedRotation = worldQuaternion.clone().multiply(rotationOffset);
 
-            handRefCollider.current.setRotation({
-                x: adjustedRotation.x,
-                y: adjustedRotation.y,
-                z: adjustedRotation.z,
-                w: adjustedRotation.w,
-            });
+    //         handRefCollider.current.setRotation({
+    //             x: adjustedRotation.x,
+    //             y: adjustedRotation.y,
+    //             z: adjustedRotation.z,
+    //             w: adjustedRotation.w,
+    //         });
 
-        }
+    //     }
 
-    })
-    ////////////////////////////////////////////////////////////////
-    const { health, setHealth } = useGameContext()
-    const [isVulnerable, setIsVulnerable] = useState(false);
-    const [canTakeDamage, setCanTakeDamage] = useState(true);
-    const enemigos = ["tentaculoBody", "tentaculoHead","Boar" , "Fuego", "Spikes", "ShadowEnemy"];
-    const hitEnemigo = new Audio(golpeado)
+    // })
+    // //////////////////////////////////////////////////////////////
+    // const { health, setHealth } = useGameContext()
+    // const [isVulnerable, setIsVulnerable] = useState(false);
+    // const [canTakeDamage, setCanTakeDamage] = useState(true);
+    // const enemigos = ["tentaculoBody", "tentaculoHead","Boar" , "Fuego", "Spikes", "ShadowEnemy"];
+    // const hitEnemigo = new Audio(golpeado)
 
 
 
-    const handleHit = (event) => {
-        const nombreEnemigo = event.colliderObject.name;
-        if (enemigos.includes(nombreEnemigo) && canTakeDamage) {
-            setIsVulnerable(true);
-            setCanTakeDamage(false)
-        }
-    };
+    // const handleHit = (event) => {
+    //     const nombreEnemigo = event.colliderObject.name;
+    //     if (enemigos.includes(nombreEnemigo) && canTakeDamage) {
+    //         setIsVulnerable(true);
+    //         setCanTakeDamage(false)
+    //     }
+    // };
 
-    const handleSensorHit = (event) => {
-        const nombreEnemigo = event.colliderObject.name;
-        if (enemigos.includes(nombreEnemigo) && canTakeDamage) {
-            setIsVulnerable(true);
-            setCanTakeDamage(false)
-        }
-    }
+    // const handleSensorHit = (event) => {
+    //     const nombreEnemigo = event.colliderObject.name;
+    //     if (enemigos.includes(nombreEnemigo) && canTakeDamage) {
+    //         setIsVulnerable(true);
+    //         setCanTakeDamage(false)
+    //     }
+    // }
 
-    useEffect(() => {
-        //console.log(health)
-        if (health <= 0) {
-            //console.log("El jugador ha perdido toda su salud y ha muerto.");
-        }
-    }, [health]);
+    // useEffect(() => {
+    //     console.log(health)
+    //     if (health <= 0) {
+    //         console.log("El jugador ha perdido toda su salud y ha muerto.");
+    //     }
+    // }, [health]);
 
-    useEffect(() => {
-        if (isVulnerable) {
-            //console.log(health)
-            hitEnemigo.volume = 0.3;
-            hitEnemigo.play();
-            setHealth(prevHealth => prevHealth - 1);
-            setIsVulnerable(false);
+    // useEffect(() => {
+    //     if (isVulnerable) {
+    //         console.log(health)
+    //         hitEnemigo.volume = 0.3;
+    //         hitEnemigo.play();
+    //         setHealth(prevHealth => prevHealth - 1);
+    //         setIsVulnerable(false);
 
-            setTimeout(() => {
-                // Esta línea de código se ejecutará después de 3000 milisegundos (3 segundos)
-                setCanTakeDamage(true);
-                // Esta línea de código se ejecutará después de 3000 milisegundos (3 segundos)
-                //
-                //console.log("Ha pasado 3 segundos");
-            }, 3000);
-        }
-    }, [canTakeDamage]);
+    //         setTimeout(() => {
+    //             Esta línea de código se ejecutará después de 3000 milisegundos (3 segundos)
+    //             setCanTakeDamage(true);
+    //             Esta línea de código se ejecutará después de 3000 milisegundos (3 segundos)
+                
+    //             console.log("Ha pasado 3 segundos");
+    //         }, 3000);
+    //     }
+    // }, [canTakeDamage]);
 
     return (
         <KeyboardControls map={keyboardMap}>
